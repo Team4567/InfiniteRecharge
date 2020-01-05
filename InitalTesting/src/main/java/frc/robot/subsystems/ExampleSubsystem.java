1322c1e9359602676c259739c6cf1aaff6f85221
jabbate19 <josephabbateny@gmail.com>
2020-01-05 00:46:54 -0500

    CTRE

--- InitalTesting/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ InitalTesting/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,12 +7,16 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
+  WPI_TalonFX leftFront, leftBack, rightFront, rightBack;
   public ExampleSubsystem() {
 
   }
